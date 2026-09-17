b969b9733aff11ce0e12bd5e2f586255c32fb465
productroom7-gif <productroom7@gmail.com>
2026-09-17 09:59:17 +0300

    Delete ddu-form-page directory

--- ddu-form-page/src/components/footer/footer.jsx
+++
@@ -1,6 +0,0 @@
-const Footer = ({ language = 'en' }) => (
-  <footer><div className="container"><div className="row"><div className="col-lg-10 offset-lg-1 col-md-12 d-flex justify-content-center footer-col"><div className="copyright">
-    <p>{language === 'pl' ? '© 2025 - Meest Group - Usługi pocztowe i transportowe. Wszelkie prawa zastrzeżone' : '© 2025 - Meest Group - Postal & Transport Services. All rights reserved'}</p>
-  </div></div></div></div></footer>
-);
-export default Footer;
--- ddu-form-page/src/config.jsx
+++
@@ -1,3 +0,0 @@
-const user = 'meest';
-
-export default user;
